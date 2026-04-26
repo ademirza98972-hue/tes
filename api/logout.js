0732dfbe43e13ae89f917052c4225715dad66711
ademirza98972-hue <ademirza98972@gmail.com>
2026-04-26 13:36:12 +0800

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Set-Cookie', 'nova_session=; Path=/; Max-Age=0');
+module.exports = function handler(req, res) {
+  res.setHeader('Set-Cookie', 'nova_session=; Path=/; HttpOnly; Secure; Max-Age=0; SameSite=Lax');
   res.redirect('/');
-}
+};
